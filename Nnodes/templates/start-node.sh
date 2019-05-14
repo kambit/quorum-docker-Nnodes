8e16ca117a856e1f9dfa81301792bfe786b2e40e
Bruce Lee <coolcode@live.com>
2019-05-14 21:57:51 +0800

    :monkey_face:

--- Nnodes/templates/start-node.sh
+++ Nnodes/templates/start-node.sh
@@ -10,7 +10,7 @@ set -e
 ### Configuration Options
 TMCONF=/qdata/tm.conf
 
-GETH_ARGS="--datadir /qdata/dd --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --nodiscover --unlock 0 --password /qdata/passwords.txt"
+GETH_ARGS="--datadir /qdata/dd --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --nodiscover --unlock 0 --raftport 50400 --password /qdata/passwords.txt"
 
 if [ ! -d /qdata/dd/geth/chaindata ]; then
   echo "[*] Mining Genesis block"
@@ -21,6 +21,14 @@ echo "[*] Starting Constellation node"
 nohup /usr/local/bin/constellation-node $TMCONF 2>> /qdata/logs/constellation.log &
 
 sleep 2
+DOWN=true
+while $DOWN; do
+  sleep 1
+  DOWN=false
+	if [ ! -S "/qdata/tm.ipc" ]; then
+    DOWN=true
+	fi
+done
 
 echo "[*] Starting node"
-PRIVATE_CONFIG=$TMCONF nohup /usr/local/bin/geth $GETH_ARGS 2>>/qdata/logs/geth.log
+PRIVATE_CONFIG=/qdata/tm.ipc nohup /usr/local/bin/geth $GETH_ARGS 2>>/qdata/logs/geth.log
